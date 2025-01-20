35696925ae55bd5d1af177f441a61c235dee7d4b
Raissa Marques <raissatkm17@gmail.com>
2025-01-19 22:32:22 -0300

    feat: adiciona estrutura inicial para projetos do curso de ADS

---
+++ curso ads/desenvolvimento web/curso1.js
@@ -0,0 +1,11 @@
+function getValue(){
+    var value = document.getElementById("name").value;
+    document.getElementById("result").innerHTML = "Bem vindo, " + value + "!";
+}
+
+function removeValue(){
+    document.getElementById("name").value;
+    document.getElementById("result").innerHTML= " ";
+
+
+}
