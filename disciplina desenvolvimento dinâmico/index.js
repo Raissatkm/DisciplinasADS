7a3b87ae3892dd7386ee96fa786b2811653a0b03
Raissa Marques <raissatkm17@gmail.com>
2025-01-27 09:24:39 -0300

    curso

---
+++ disciplina desenvolvimento dinâmico/index.js
@@ -0,0 +1,43 @@
+/*Fundamentos javascript
+variaveis: palavras chave var, let ou const.
+operadores: aritméticos, atribuição, comparação e lógico.
+estruturas de controle: laçõs de repetição, loop
+funções:blocos de codigos reutilizáveis
+objetos: entidades do mundo real e propriedades 
+
+
+var: o escopo varia sendo de uma função ou global
+let: O escopo é de bloco 
+const: Valor fixo
+
+
+
+*/
+const frase = "Olá, Eu sou uma constante global";
+
+function exemploVar(){
+    if(true){
+        var mensagem= "Olá, Faculdade descomplica! Eu sou uma var...."
+    }
+    console.log(mensagem);
+}
+
+exemploVar();
+
+
+
+function exemploLet(){
+    if(true){
+        let mensagem= "Olá, mundo!"
+        console.log(mensagem)
+    }
+}
+exemploLet()
+
+
+function exemploConstante(){
+  const mensagem = "olá, Faculdade descomplica!";
+  console.log(mensagem);
+}
+
+exemploConstante()
