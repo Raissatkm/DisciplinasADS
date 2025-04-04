c3901cc5abc392c5c13139274e9bc7e9c41247a1
Raissa Marques <raissatkm17@gmail.com>
2025-04-04 14:38:55 -0300

    Tipos de dados

---
+++ logica de programação/TiposDados.java
@@ -0,0 +1,11 @@
+public class TiposDados {
+    public static void main(String[] args) {
+        System.out.println("Nome: João");
+        System.out.print("Idade: ");
+        System.out.println(4);
+        System.out.print("Altura: ");
+        System.out.println(1.80);
+        System.out.print("Criança:");
+        System.out.println(true);
+    }
+}
