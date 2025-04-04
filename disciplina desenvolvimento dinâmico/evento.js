c31689faea6132a40fab6e13dd71c411eef2ef22
Raissa Marques <raissatkm17@gmail.com>
2025-04-04 13:53:57 -0300

    .

---
+++ disciplina desenvolvimento dinâmico/evento.js
@@ -0,0 +1,11 @@
+/*
+Eventos de mouse 
+.click: acionado quando o usuário clica em um elemento
+.mouseover: acionado quando o ponteiro do mouse se move sobre um elemento 
+.mouseout: acionado quando o ponteiro do mouse sai de um elemento
+.mousedown: acionado quando o botão do mouse é precionado sobre um elemento 
+.mouseup: acionado quando o botão do mouse é solto sobre um elemento
+*/
+
+
+
