921e2a9ad2f77575bfbcb7213640401727f323da
Raissa Marques <raissatkm17@gmail.com>
2025-04-06 16:33:42 -0300

    aula2

---
+++ logica de programação/SomaMedia.java
@@ -0,0 +1,24 @@
+
+
+public class SomaMedia{
+    public static void main(String[] args) {
+        String nomeAlg;
+        int resultadoSoma;
+        float resultadoMedia = 0;
+        float resultadoMedia2;
+
+        nomeAlg = "Soma e Média";
+        resultadoSoma = 5+5+5;
+        resultadoMedia2= (5+5+5)/3;
+
+        System.out.println("Soma e média dos números 5,5 e 5");
+        System.out.println("Nome do Algoritmo: ");
+        System.out.println("Resultado da Soma: ");
+        System.out.print(resultadoSoma);
+        System.out.println("Resultado da Média: ");
+        System.out.print(resultadoMedia);
+        System.out.print("Resultado da Média usando a variável resultadoSoma: ");
+        System.out.println(resultadoMedia2);
+
+}
+}
